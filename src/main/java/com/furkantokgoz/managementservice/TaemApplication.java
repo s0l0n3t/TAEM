a01759c6a943bcb69ecf8dcb004a693b9e7b1376
furkantkgz <furkantokgz@gmail.com>
2026-02-14 18:41:04 +0300

    Springboot new project reached.

---
+++ src/main/java/com/furkantokgoz/managementservice/TaemApplication.java
@@ -0,0 +1,13 @@
+package com.furkantokgoz.managementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaemApplication.class, args);
+    }
+
+}
